cfaeab06d8e4991b1d9c25073593b19944ce7f5e
Runal <118291026+Runal21@users.noreply.github.com>
2024-04-09 01:10:16 +0530

    trying vercel deployment2

--- DESSoln/settings.py
+++ DESSoln/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static','staticfiles_build')
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
 # MEDIA_URL = "/media/"  
